47ef952a3e5d68b4883b314f41ad2bb96b41ac80
Chanon-SVN <chanon@kmi.tl>
2020-05-04 17:40:38 +0700

    Add Modal Show/Close in Class Component

--- src/App.js
+++ src/App.js
@@ -8,13 +8,18 @@ import Col from 'react-bootstrap/Col';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Image from 'react-bootstrap/Image';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const testData = [
-  {img_url:"https://avatars0.githubusercontent.com/u/810438?v=4", name:"Dan Abramov", industry:"IT", email:"dan_abramov@gmail.com", phone:"021234567" },
-  {img_url:"https://avatars2.githubusercontent.com/u/6820?v=4",  name:"Sophie Alpert", industry:"IT", email:"sophie@gmail.com", phone:"0823857839" },
-  {img_url:"https://avatars2.githubusercontent.com/u/63648?v=4", name:"Sebastian Markage", industry:"IT", email:"Sebastian_markage@gmail.com", phone:"0832462833" }
+  {id:"24234", img_url:"https://avatars0.githubusercontent.com/u/810438?v=4", name:"Dan Abramov", industry:"IT", email:"dan_abramov@gmail.com", phone:"021234567" },
+  {id:"53435", img_url:"https://avatars2.githubusercontent.com/u/6820?v=4",  name:"Sophie Alpert", industry:"IT", email:"sophie@gmail.com", phone:"0823857839" },
+  {id:"02348", img_url:"https://avatars2.githubusercontent.com/u/63648?v=4", name:"Sebastian Markage", industry:"IT", email:"Sebastian_markage@gmail.com", phone:"0832462833" }
 ]
 
 class Search extends React.Component {
@@ -33,6 +38,105 @@ class Search extends React.Component {
   }
 };
 
+class AddPartner extends React.Component {
+  constructor (props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this)
+    this.handleClsoe = this.handleClose.bind(this)
+
+    this.state = {show:false}
+  }
+
+  handleShow = () => {
+    this.setState({show:true})
+  }
+
+  handleClose = () => {
+    this.setState({show:false})
+  }
+  render() {
+      return (
+        <div>
+          <Button variant="primary" onClick={this.handleShow}>
+            +Add Partner
+          </Button>
+          {/* Start Modal*/}
+          <Modal show={this.state.show} onHide={this.handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Add partner</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group controlId="name">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control value={this.state.name} onChange={event => this.setState({name:event.target.value})} type="text" placeholder="Please Enter a fullname" required/>
+                  <Form.Text className="text-muted">
+                    Please enter full name.
+                  </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="industry">
+                  <Form.Label>Industry</Form.Label>
+                  <Form.Control type="text" placeholder="Please enter an industry" required/>
+                </Form.Group>
+                <Form.Group controlId="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="Please enter an email" required/>
+                  <Form.Text className="text-muted">
+                    we'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="number">
+                  <Form.Label>Phone Number</Form.Label>
+                  <Form.Control type="text" placeholder="Please enter a phone number" required/>
+                </Form.Group>
+                <Form.Group controlId="img_url">
+                  <Form.Label>Image Url</Form.Label>
+                  <Form.Control type="text" placeholder="Please enter an image url" required/>
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={this.handleSubmit}>
+                Add Partner
+              </Button>
+            </Modal.Footer>
+          </Modal>
+          {/* End Modal*/}
+        </div>
+      );
+    }
+}
+
+class PartnerNavbar extends React.Component {
+  state = {name:'', industry:''}
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.sate.name);
+  }
+  render() {
+
+    return (
+      <Navbar fixed="top" bg="light" expand="lg">
+        <Navbar.Brand href="#home">Partner List</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+          <AddPartner />
+
+        </Navbar.Collapse>
+        </Navbar>
+    );
+  }
+}
+
 class DataTable extends React.Component {
   render() {
     const profile = this.props;
@@ -40,11 +144,11 @@ class DataTable extends React.Component {
     return (
         <tbody>
           <tr>
-            <td className="text-center"><Image src={profile.img_url} style={{width:50}} roundedCircle/></td>
-            <td className="text-center">{profile.name}</td>
-            <td className="text-center">{profile.industry}</td>
-            <td className="text-center">{profile.email}</td>
-            <td className="text-center">{profile.phone}</td>
+            <td className="text-center" key={profile.img_url}><Image src={profile.img_url} style={{width:50}} roundedCircle/></td>
+            <td className="text-center" key={profile.name}>{profile.name}</td>
+            <td className="text-center" key={profile.industry}>{profile.industry}</td>
+            <td className="text-center" key={profile.email}>{profile.email}</td>
+            <td className="text-center" key={profile.phone}>{profile.phone}</td>
           </tr>
         </tbody>
     );
@@ -65,7 +169,7 @@ class PartnerTable extends React.Component {
             <th className="text-center">Phone Number</th>
           </tr>
         </thead>
-        {testData.map(profile => <DataTable{...profile}/>)}
+        {testData.map(profile => <DataTable key={profile.id} {...profile}/>)}
       </Table>
     );
   }
@@ -74,21 +178,24 @@ class PartnerTable extends React.Component {
 
 function App() {
   return (
-    <Container>
-      <Row style={{marginTop:60}}>
-        <Col>
-          <Row>
+    <div>
+      <PartnerNavbar />
+      <Container>
+        <Row style={{marginTop:60}}>
           <Col>
-            <h3>Partners</h3>
+            <Row>
+              <Col>
+                <h3>Partners</h3>
+              </Col>
+              <Col>
+                <Search />
+              </Col>
+            </Row>
+            <PartnerTable />
           </Col>
-          <Col>
-            <Search />
-          </Col>
-          </Row>
-          <PartnerTable />
-        </Col>
-      </Row>
-    </Container>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
